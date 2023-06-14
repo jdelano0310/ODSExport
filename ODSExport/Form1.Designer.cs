ebb7370a6b1295acd9a34e4ff061d944cb163ed6
Jon <jd31068@hotmail.com>
2023-06-14 11:59:15 -0400

    Add project files.

---
+++ ODSExport/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ODSExport
+{
+	partial class frmMain
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label1 = new Label();
+			txtFileName = new TextBox();
+			btnOpenFileDLG = new Button();
+			dgvSheet = new DataGridView();
+			drpSheetNames = new ComboBox();
+			label2 = new Label();
+			oFDLG = new OpenFileDialog();
+			lblStatus = new Label();
+			chkFirstRowColumnNames = new CheckBox();
+			btnExport = new Button();
+			((System.ComponentModel.ISupportInitialize)dgvSheet).BeginInit();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(12, 9);
+			label1.Name = "label1";
+			label1.Size = new Size(189, 15);
+			label1.TabIndex = 0;
+			label1.Text = "Select Only Office Spreadsheet File";
+			// 
+			// txtFileName
+			// 
+			txtFileName.Location = new Point(12, 27);
+			txtFileName.Name = "txtFileName";
+			txtFileName.ReadOnly = true;
+			txtFileName.Size = new Size(726, 23);
+			txtFileName.TabIndex = 1;
+			// 
+			// btnOpenFileDLG
+			// 
+			btnOpenFileDLG.Location = new Point(744, 27);
+			btnOpenFileDLG.Name = "btnOpenFileDLG";
+			btnOpenFileDLG.Size = new Size(44, 23);
+			btnOpenFileDLG.TabIndex = 2;
+			btnOpenFileDLG.Text = "...";
+			btnOpenFileDLG.UseVisualStyleBackColor = true;
+			btnOpenFileDLG.Click += btnOpenFileDLG_Click;
+			// 
+			// dgvSheet
+			// 
+			dgvSheet.AllowUserToAddRows = false;
+			dgvSheet.AllowUserToDeleteRows = false;
+			dgvSheet.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			dgvSheet.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dgvSheet.Location = new Point(12, 86);
+			dgvSheet.MultiSelect = false;
+			dgvSheet.Name = "dgvSheet";
+			dgvSheet.ReadOnly = true;
+			dgvSheet.RowTemplate.Height = 25;
+			dgvSheet.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+			dgvSheet.Size = new Size(776, 352);
+			dgvSheet.TabIndex = 3;
+			// 
+			// drpSheetNames
+			// 
+			drpSheetNames.DropDownStyle = ComboBoxStyle.DropDownList;
+			drpSheetNames.FormattingEnabled = true;
+			drpSheetNames.Location = new Point(68, 58);
+			drpSheetNames.Name = "drpSheetNames";
+			drpSheetNames.Size = new Size(202, 23);
+			drpSheetNames.TabIndex = 4;
+			drpSheetNames.SelectedIndexChanged += drpSheetNames_SelectedIndexChanged;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(12, 61);
+			label2.Name = "label2";
+			label2.Size = new Size(41, 15);
+			label2.TabIndex = 5;
+			label2.Text = "Sheets";
+			// 
+			// oFDLG
+			// 
+			oFDLG.DefaultExt = "*.ods | Open Document Spreadsheet";
+			oFDLG.FileName = "*.ods";
+			// 
+			// lblStatus
+			// 
+			lblStatus.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+			lblStatus.ForeColor = Color.Blue;
+			lblStatus.Location = new Point(465, 6);
+			lblStatus.Name = "lblStatus";
+			lblStatus.Size = new Size(323, 18);
+			lblStatus.TabIndex = 6;
+			// 
+			// chkFirstRowColumnNames
+			// 
+			chkFirstRowColumnNames.AutoSize = true;
+			chkFirstRowColumnNames.Location = new Point(281, 62);
+			chkFirstRowColumnNames.Name = "chkFirstRowColumnNames";
+			chkFirstRowColumnNames.Size = new Size(201, 19);
+			chkFirstRowColumnNames.TabIndex = 7;
+			chkFirstRowColumnNames.Text = "First row contains column names";
+			chkFirstRowColumnNames.UseVisualStyleBackColor = true;
+			chkFirstRowColumnNames.CheckedChanged += chkFirstRowColumnNames_CheckedChanged;
+			// 
+			// btnExport
+			// 
+			btnExport.Location = new Point(695, 62);
+			btnExport.Name = "btnExport";
+			btnExport.Size = new Size(93, 23);
+			btnExport.TabIndex = 8;
+			btnExport.Text = "Export to CVS";
+			btnExport.UseVisualStyleBackColor = true;
+			btnExport.Click += btnExport_Click;
+			// 
+			// frmMain
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(btnExport);
+			Controls.Add(chkFirstRowColumnNames);
+			Controls.Add(lblStatus);
+			Controls.Add(label2);
+			Controls.Add(drpSheetNames);
+			Controls.Add(dgvSheet);
+			Controls.Add(btnOpenFileDLG);
+			Controls.Add(txtFileName);
+			Controls.Add(label1);
+			Name = "frmMain";
+			StartPosition = FormStartPosition.CenterScreen;
+			Text = "ODS Sheet Exporter";
+			((System.ComponentModel.ISupportInitialize)dgvSheet).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label label1;
+		private TextBox txtFileName;
+		private Button btnOpenFileDLG;
+		private DataGridView dgvSheet;
+		private ComboBox drpSheetNames;
+		private Label label2;
+		private OpenFileDialog oFDLG;
+		private Label lblStatus;
+		private CheckBox chkFirstRowColumnNames;
+		private Button btnExport;
+	}
+}
---
+++ ODSExport/Form1.cs
@@ -0,0 +1,225 @@
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+using Zaretto.ODS;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ODSExport
+{
+	public partial class frmMain : Form
+	{
+		private System.Data.DataSet? spreadsheetData;
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void UpdateStatus(string statusMSG)
+		{
+			lblStatus.Text = statusMSG;
+			Application.DoEvents();
+
+		}
+
+		private void btnOpenFileDLG_Click(object sender, EventArgs e)
+		{
+			if (oFDLG.ShowDialog() == DialogResult.OK)
+			{
+				txtFileName.Text = oFDLG.FileName;
+				Application.DoEvents();
+
+				UpdateStatus("Reading file");
+				ReadODSFile(oFDLG.FileName);
+
+				try
+				{
+					txtFileName.BackColor = SystemColors.Window;
+				}
+				catch (Exception ex)
+				{
+					txtFileName.BackColor = Color.Red;
+					MessageBox.Show($"Error opening file: {ex.Message}\n\n" +
+					$"Details:\n\n{ex.StackTrace}");
+				}
+				UpdateStatus("");
+			}
+		}
+
+		private void ReadODSFile(string selectedFileName)
+		{
+
+			var odsReaderWriter = new ODSReaderWriter();
+			spreadsheetData = odsReaderWriter.ReadOdsFile(selectedFileName);
+
+			dgvSheet.Rows.Clear();
+			drpSheetNames.Items.Clear();
+
+			// get the sheets that are available in this file
+			if (spreadsheetData != null)
+			{
+				drpSheetNames.Items.Add("Select a sheet to export");
+				foreach (DataTable table in spreadsheetData.Tables)
+				{
+					drpSheetNames.Items.Add(table.TableName);
+				}
+				drpSheetNames.SelectedIndex = 0;
+			}
+			else
+			{
+				txtFileName.BackColor = Color.Red;
+				MessageBox.Show("Unable to find any spreadsheet objects in this file.");
+			}
+		}
+
+		private void drpSheetNames_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			// after the user selects a sheet - load it into the grid
+			if (drpSheetNames.SelectedIndex < 1) { return; }
+
+			UpdateStatus("Loading sheet");
+
+			foreach (DataTable table in spreadsheetData.Tables)
+			{
+				if (table.TableName == drpSheetNames.Text)
+				{
+					if (!chkFirstRowColumnNames.Checked)
+					{
+						// the data doesn't include the column names in the first row
+						// the current table can be added to teh grid using generic 
+						// column headers
+						dgvSheet.Columns.Clear();
+						dgvSheet.Rows.Clear();
+
+						dgvSheet.DataSource = table;
+
+					}
+					else
+					{
+						// the user has indicated that the data contains a head row
+						dgvSheet.DataSource = null;
+						dgvSheet.Columns.Clear();
+						dgvSheet.Rows.Clear();
+
+						var rownum = 1;
+						foreach (var row in table.AsEnumerable())
+						{
+							if (rownum == 1)
+							{
+								// this is the header row, add these as columns
+								for (int i = 0; table.Columns.Count > i; i++)
+								{
+									DataGridViewColumn newCol = new DataGridViewColumn(); // add a column to the grid
+									DataGridViewCell cell = new DataGridViewTextBoxCell(); //Specify which type of cell in this column
+									newCol.CellTemplate = cell;
+
+									newCol.HeaderText = row.ItemArray[i].ToString();
+									newCol.Name = row.ItemArray[i].ToString();
+									newCol.Visible = true;
+									newCol.Width = 100;
+
+									dgvSheet.Columns.Add(newCol);
+								}
+							}
+							else
+							{
+								// row of data
+								var dgvRow = new DataGridViewRow();
+								var newRowIndex = dgvSheet.Rows.Add();
+
+								for (int i = 0; table.Columns.Count > i; i++)
+								{
+									dgvSheet.Rows[newRowIndex].Cells[i].Value = row.ItemArray[i].ToString();
+								}
+
+							}
+							rownum++;
+						}
+					}
+					UpdateStatus("");
+					return;
+				}
+
+			}
+		}
+
+		private void chkFirstRowColumnNames_CheckedChanged(object sender, EventArgs e)
+		{
+			drpSheetNames_SelectedIndexChanged(sender, e);
+		}
+
+		private void btnExport_Click(object sender, EventArgs e)
+		{
+			if (dgvSheet.Rows.Count > 0)
+			{
+				SaveFileDialog sfd = new SaveFileDialog();
+				sfd.Filter = "CSV (*.csv)|*.csv";
+				sfd.FileName = "Output.csv";
+				bool fileError = false;
+				if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					if (File.Exists(sfd.FileName))
+					{
+						try
+						{
+							File.Delete(sfd.FileName);
+						}
+						catch (IOException ex)
+						{
+							fileError = true;
+							MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+						}
+					}
+					if (!fileError)
+					{
+						try
+						{
+							int columnCount = dgvSheet.Columns.Count;
+							string columnNames = "";
+							string[] outputCsv = new string[dgvSheet.Rows.Count + 1];
+
+							if (chkFirstRowColumnNames.Checked)
+							{
+								// include the column names in the export
+								for (int i = 0; i < columnCount; i++)
+								{
+									columnNames += dgvSheet.Columns[i].HeaderText.ToString() + ",";
+								}
+								outputCsv[0] += columnNames;
+							}
+
+							for (int i = 1; (i - 1) < dgvSheet.Rows.Count; i++)
+							{
+								for (int j = 0; j < columnCount; j++)
+								{
+									if (dgvSheet.Rows[i - 1].Cells[j].Value.ToString().Contains(','))
+									{
+										// the value contains a , so it needs to be surrounded by quotes
+										outputCsv[i] += $"\"{dgvSheet.Rows[i - 1].Cells[j].Value.ToString()}\",";
+									}
+									else
+									{
+										outputCsv[i] += dgvSheet.Rows[i - 1].Cells[j].Value.ToString() + ",";
+									}
+
+								}
+							}
+
+							File.WriteAllLines(sfd.FileName, outputCsv, Encoding.UTF8);
+							MessageBox.Show("Data Exported Successfully !!!", "Info");
+						}
+						catch (Exception ex)
+						{
+							MessageBox.Show("Error :" + ex.Message);
+						}
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("No Record To Export !!!", "Info");
+			}
+		}
+	}
+}
